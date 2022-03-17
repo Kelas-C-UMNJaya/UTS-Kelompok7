69937a2ab159266808d49c4c9ba293f26e936384
jonathanstyo <100742241+jonathanstyo@users.noreply.github.com>
2022-03-17 11:32:03 +0700

    Update kel 7

--- script.js
+++ script.js
@@ -20,3 +20,83 @@ function setImg() {
     return slider_img.setAttribute('src', 'images/' + images[i]);
 }
 
+function gameStart(){
+    var name = document.getElementById("nama").value;
+    
+    document.getElementById("avatar").classList.add("d-none");
+    document.getElementById("game").classList.remove("d-none");
+}
+
+
+var hunger = document.getElementById("hunger");
+var health = document.getElementById("health");
+var happiness = document.getElementById("happiness");
+var knowledge = document.getElementById("knowledge");
+
+function makanIncrease(){
+    hunger.style.width = (parseInt(hunger.style.width) + 20) + "%";
+    happiness.style.width = (parseInt(happiness.style.width) - 10) + "%";
+    hunger.ariaValueNow = (parseInt(hunger.ariaValueNow) + 20);
+    happiness.ariaValueNow = (parseInt(happiness.ariaValueNow) - 10);
+}
+
+function tidurIncrease(){
+    health.style.width = (parseInt(health.style.width) + 20) + "%";
+    hunger.style.width = (parseInt(hunger.style.width) - 10) + "%";
+    hunger.ariaValueNow = (parseInt(hunger.ariaValueNow) - 10);
+    health.ariaValueNow = (parseInt(health.ariaValueNow) + 20);
+}
+
+function mainIncrease(){
+    if (health.ariaValueNow < 20 ) {
+        happiness.style.width = (parseInt(happiness.style.width) + 5) + "%";
+        happiness.ariaValueNow = (parseInt(happiness.ariaValueNow) + 5);
+    } else if (happiness.ariaValueNow < 100 && happiness.ariaValueNow > 20){
+        happiness.style.width = (parseInt(happiness.style.width) + 20) + "%";
+        health.style.width = (parseInt(health.style.width) - 10) + "%";
+        health.ariaValueNow = (parseInt(health.ariaValueNow) - 10);
+        happiness.ariaValueNow = (parseInt(happiness.ariaValueNow) + 20);
+    }
+
+    
+    if(health.ariaValueNow >= 99 ) {
+        happiness.style.width = 99 + "%";
+        happiness.ariaValueNow= 99;
+    }
+    
+}
+
+function belajarIncrease(){
+    if (hunger.ariaValueNow < 20 ) {
+        knowledge.style.width = (parseInt(knowledge.style.width) + 20) + "%";
+        happiness.style.width = (parseInt(happiness.style.width) - 10) + "%";
+        health.style.width = (parseInt(health.style.width) - 10) + "%";
+        happiness.ariaValueNow = (parseInt(happiness.ariaValueNow) - 10);
+        health.ariaValueNow = (parseInt(health.ariaValueNow) - 10);
+        knowledge.ariaValueNow = (parseInt(knowledge.ariaValueNow) + 20);
+        
+    } else {
+        knowledge.style.width = (parseInt(knowledge.style.width) + 20) + "%";
+        hunger.style.width = (parseInt(hunger.style.width) - 10) + "%";
+        hunger.ariaValueNow = (parseInt(hunger.ariaValueNow) - 10);
+        knowledge.ariaValueNow = (parseInt(knowledge.ariaValueNow) + 20);
+    }
+    
+}
+
+function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var session = document.getElementById('session');
+
+    if(hrs >= 12){
+        session.innerHTML = 'PM';
+    }else{
+        session.innerHTML = 'AM';
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+}
+setInterval(displayTime, 10);
