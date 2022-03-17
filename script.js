aa31238f0c82e3c549de86bd31153e9721ea946c
LeonardoJFN <100654621+LeonardoJFN@users.noreply.github.com>
2022-03-17 18:39:13 +0800

    rework action button and added random character select

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 
+let hunger = 50
+let health = 50
+let happiness = 50
+let knowledge = 0
 var slider_img = document.querySelector('.slider-img');
 var images = ['char1.png', 'char2.png'];
 var i = 0;
@@ -19,27 +23,53 @@ function next(){
 function setImg() {
     return slider_img.setAttribute('src', 'images/' + images[i]);
 }
+  
+  function randomAvatar(){
+    random_index = Math.floor(Math.random() * images.length);
+
+    selected_image = images[random_index]
+  
+    document.getElementById('image_shower').src = `./images/${selected_image}`
+  }
+
+
+
 
 function gameStart(){
-    var name = document.getElementById("nama").value;
+    let playerName = document.getElementById("nama").value;
     
     document.getElementById("avatar").classList.add("d-none");
     document.getElementById("game").classList.remove("d-none");
+    document.getElementById("username").innerHTML = playerName;
+    progressBar("hunger", hunger);
+    progressBar("health", health);
+    progressBar("happiness", happiness);
+    progressBar("knowledge", knowledge);
+
+
 }
 
 
-var hunger = document.getElementById("hunger");
-var health = document.getElementById("health");
-var happiness = document.getElementById("happiness");
-var knowledge = document.getElementById("knowledge");
 
+
+function progressBar  (id, value)  {
+    const progressBar = document.getElementById(id);
+    //let cls = "progress-bar";
+    //cls += " " + id;
+  
+    progressBar.setAttribute("style", "width: " + value + "%");
+    //progressBar.setAttribute("class", cls);
+    progressBar.innerHTML = value;
+  };
 function makanIncrease(){
-    hunger.style.width = (parseInt(hunger.style.width) + 20) + "%";
-    happiness.style.width = (parseInt(happiness.style.width) - 10) + "%";
-    hunger.ariaValueNow = (parseInt(hunger.ariaValueNow) + 20);
-    happiness.ariaValueNow = (parseInt(happiness.ariaValueNow) - 10);
+    hunger += 20;
+    main -= 10;
+    if(hunger > 100){
+        hunger = 100;
+    }
+    progressBar("hunger", hunger);
 }
-
+/*
 function tidurIncrease(){
     health.style.width = (parseInt(health.style.width) + 20) + "%";
     hunger.style.width = (parseInt(hunger.style.width) - 10) + "%";
@@ -99,4 +129,4 @@ function displayTime(){
     document.getElementById('hours').innerHTML = hrs;
     document.getElementById('minutes').innerHTML = min;
 }
-setInterval(displayTime, 10);
+setInterval(displayTime, 10);*/
